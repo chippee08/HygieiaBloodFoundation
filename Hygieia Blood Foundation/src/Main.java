223a988fb75a7d600a6ab6037b074a1ecbd5628e
chippee08 <93817250+chippee08@users.noreply.github.com>
2022-12-03 22:13:40 +0800

    Update Main.java

--- Hygieia Blood Foundation/src/Main.java
+++ Hygieia Blood Foundation/src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 
 public class Main {
 
@@ -6,6 +7,29 @@ public class Main {
 		UserAndPass userNpass = new UserAndPass();
 		
 		LoginPage loginPage = new LoginPage(userNpass.getLoginInfo());
+		
+		try {
+			//Getting connection to database
+			Connection conn = DriverManager.getConnection("jdbc::mysql://localhost:3306/hygieia_foundation", "root", "joemama0008");
+			//Create a statement
+			Statement myStmt = conn.createStatement();
+			//Execute sql query
+			ResultSet myRs = myStmt.executeQuery("Select * from tbl_students;");
+			//process the result set
+			while(myRs.next());
+			System.out.println(myRs.getString("LastName") + ", " + myRs.getString("FirstName"));
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+		System.out.println("Loading driver...");
+
+		try {
+		    Class.forName("com.mysql.jdbc.Driver");
+		    System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+		    throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+		}
 
 	}
 
